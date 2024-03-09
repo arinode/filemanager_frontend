e69a8981d93edf0156443c0f3fbafe58f5394cfc
arinode <arinode@proton.me>
2024-03-09 09:36:41 +0100

    add header mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Sidebar from './Sidebar'
+import Header from './Header'
 
 function App() {
     return (
         <>
             <Sidebar />
             <div id="filemanager-wrapper">
-                <header></header>
+                <Header></Header>
                 <main></main>
             </div>
         </>
---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+function Header() {
+    return (
+        <header>
+            <button>Show sidebar</button>
+            <input type="search" placeholder="Search"></input>
+        </header>
+    )
+}
+
+export default Header
