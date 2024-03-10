ba15bbd367b2202a471f9ffc8e364aa065e2d5f5
arinode <arinode@proton.me>
2024-03-10 22:41:19 +0100

    TextInput add new component

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,14 @@
+import './TextInput.css'
+
+export interface TextInputProps {
+    placeholder?: string;
+    disabled?: boolean;
+    type?: "text" | "search";
+    onInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function TextInput({ placeholder = " · · · ", disabled = false, type = "text", onInput }: TextInputProps) {
+    return (
+        <input className='text-input' type={type} onInput={onInput} placeholder={placeholder} disabled={disabled} />
+    )
+}
