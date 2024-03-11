89edae56f50799598973b7f265b345634408fa43
arinode <arinode@proton.me>
2024-03-11 11:07:36 +0100

    Toolbar.tsx use Breadcrumbs component

--- src/Toolbar.tsx
+++ src/Toolbar.tsx
@@ -1,10 +1,11 @@
 import './Toolbar.css'
+import Breadcrumbs from './components/Breadcrumbs'
 import Button from './components/Button'
 
 function Toolbar() {
     return (
         <div className="toolbar">
-            <nav>home user Downloads</nav>
+            <Breadcrumbs />
             <menu>
                 <li><Button>New directory</Button></li>
                 <li><Button>Change view</Button></li>
