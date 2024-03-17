fb3a3c655a93eb7e1b087b885144c19430c5b084
arinode <arinode@proton.me>
2024-03-17 19:19:37 +0100

    feat: Header make search field controlled

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import './Header.css';
 import Button from './components/Button';
 import TextInput from './components/TextInput';
 
 const Header = ({ onSidebarButtonClick, isSidebarButtonVisible }) => {
   const style = isSidebarButtonVisible ? {} : { display: 'none' };
+  const [query, setQuery] = useState('');
+
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    setQuery(e.target.value);
+  };
 
   return (
     <header>
       <Button kind='flat' style={style} onClick={onSidebarButtonClick}>
         Show sidebar
       </Button>
-      <TextInput placeholder='Search' onChange={(e) => console.log(e)} />
+      <TextInput placeholder='Search' onChange={onSearchChange} value={query} />
     </header>
   );
 };
