08a0920fb66dca360eb192049dbdd5590bbf8af2
arinode <arinode@proton.me>
2024-03-28 20:30:42 +0100

    feat: load files form api in FileBrowser

--- src/app/FileBrowser.tsx
+++ src/app/FileBrowser.tsx
@@ -1,12 +1,27 @@
 import './FileBrowser.css';
 
 import { DirEntryTable } from '../components';
-import { getTestEntries } from '../direntry';
+import { useGetEntryChildrenQuery } from '../features/api/apiSlice';
 
 const FileBrowser = () => {
+  const { data: children, isLoading, isFetching, isError } =
+    useGetEntryChildrenQuery(
+      '/',
+    );
+
+  console.log(children);
+
+  if (isLoading || isFetching || children === undefined) {
+    return 'loading';
+  }
+
+  if (isError) {
+    return 'error';
+  }
+
   return (
     <div className='file-browser'>
-      <DirEntryTable entries={getTestEntries(64)} />
+      <DirEntryTable entries={children.children} />
     </div>
   );
 };
