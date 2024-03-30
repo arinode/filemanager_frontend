d8da193507445cc1f98fe365e6437c52137be20a
arinode <arinode@proton.me>
2024-03-30 14:11:12 +0100

    feat: add root router

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,19 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
