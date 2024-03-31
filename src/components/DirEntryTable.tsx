4ccf53d108542c25e5c382f99027ffd8891a5b92
arinode <arinode@proton.me>
2024-03-31 22:49:45 +0200

    feat: allow to choose which table columns are shown in DirEntryTable

--- src/components/DirEntryTable.tsx
+++ src/components/DirEntryTable.tsx
@@ -4,41 +4,65 @@ import { formatBytes, formatUnixTimestamp } from '../utils';
 import { Button } from './index';
 import { EntryMetadata } from '../types';
 
+type EntryField = 'basename' | 'size' | 'created' | 'modified';
+
 export interface DirEntryTableProps {
   entries?: EntryMetadata[];
   onEntryClick?: (entry: EntryMetadata) => void;
   disabled?: boolean;
+  visibleFields?: EntryField[];
 }
 
 export const DirEntryTable = (
-  { entries = [], disabled = false, onEntryClick = () => {} }:
-    DirEntryTableProps,
+  {
+    entries = [],
+    disabled = false,
+    onEntryClick = () => {},
+    visibleFields = ['basename', 'size', 'modified'],
+  }: DirEntryTableProps,
 ) => {
   const tableRows = entries.map((e) => {
     return (
       <DirEntryTableRow
         entry={e}
         onClick={() => onEntryClick(e)}
+        visibleFields={visibleFields}
       />
     );
   });
+
+  const thBasename = (
+    <th>
+      <Button kind='flat'>Name</Button>
+    </th>
+  );
+
+  const thCreated = (
+    <th className='date'>
+      <Button kind='flat'>Created</Button>
+    </th>
+  );
+
+  const thModified = (
+    <th className='date'>
+      <Button kind='flat'>Modified</Button>
+    </th>
+  );
+
+  const thSize = (
+    <th className='size'>
+      <Button kind='flat'>Size</Button>
+    </th>
+  );
 
   return (
     <table className={`dir-entry-table ${disabled ? 'disabled' : ''}`}>
       <thead>
         <tr>
-          <th>
-            <Button kind='flat'>Name</Button>
-          </th>
-          <th className='date'>
-            <Button kind='flat'>Created</Button>
-          </th>
-          <th className='date'>
-            <Button kind='flat'>Modified</Button>
-          </th>
-          <th className='size'>
-            <Button kind='flat'>Size</Button>
-          </th>
+          {visibleFields.includes('basename') ? thBasename : null}
+          {visibleFields.includes('created') ? thCreated : null}
+          {visibleFields.includes('modified') ? thModified : null}
+          {visibleFields.includes('size') ? thSize : null}
         </tr>
       </thead>
       <tbody>
@@ -51,19 +75,24 @@ export const DirEntryTable = (
 export interface DirEntryTableRowProps {
   entry: EntryMetadata;
   onClick: React.MouseEventHandler<HTMLTableRowElement>;
+  visibleFields: EntryField[];
 }
 
 const DirEntryTableRow = (
-  { entry, onClick }: DirEntryTableRowProps,
+  { entry, onClick, visibleFields }: DirEntryTableRowProps,
 ) => {
   const { basename, created, size, modified } = entry;
 
   return (
     <tr onClick={onClick}>
-      <td>{basename}</td>
-      <td>{formatUnixTimestamp(created)}</td>
-      <td>{formatUnixTimestamp(modified)}</td>
-      <td>{formatBytes(size)}</td>
+      {visibleFields.includes('basename') ? <td>{basename}</td> : null}
+      {visibleFields.includes('created')
+        ? <td>{formatUnixTimestamp(created)}</td>
+        : null}
+      {visibleFields.includes('modified')
+        ? <td>{formatUnixTimestamp(modified)}</td>
+        : null}
+      {visibleFields.includes('size') ? <td>{formatBytes(size)}</td> : null}
     </tr>
   );
 };
