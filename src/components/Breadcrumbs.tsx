a239007ac672cd5e3e84079824f6a9c59ff0e6ef
arinode <arinode@proton.me>
2024-05-17 10:00:40 +0200

    refactor: Breadcrumbs remove unused import

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './Breadcrumbs.css';
 import { Button } from './Button';
 
